8be75eb02fba3623dc3f6cc594f9408e230b162f
richard-epsilla <richard@epsilla.com>
2023-09-28 14:00:21 -0700

    local hosted chatbot

---
+++ local-chatbot/app.py
@@ -0,0 +1,67 @@
+from langchain.vectorstores import Epsilla
+from pyepsilla import vectordb
+from sentence_transformers import SentenceTransformer
+import streamlit as st
+
+import subprocess
+from typing import List
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+class LocalEmbeddings():
+  def embed_query(self, text: str) -> List[float]:
+    return model.encode(text).tolist()
+
+embeddings = LocalEmbeddings()
+
+client = vectordb.Client()
+vector_store = Epsilla(
+  client,
+  embeddings,
+  db_path="/tmp/localchatdb",
+  db_name="LocalChatDB"
+)
+vector_store.use_collection("LocalChatCollection")
+
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+  st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+
+for msg in st.session_state.messages:
+  st.chat_message(msg["role"]).write(msg["content"])
+
+if question := st.chat_input():
+  st.session_state.messages.append({"role": "user", "content": question})
+
+  context = '\n'.join(map(lambda doc: doc.page_content, vector_store.similarity_search(question)))
+
+  st.chat_message("user").write(question)
+  prompt = f'''
+    Answer the Question below based on the given Context below. Try to understand the Context and rephrase them.
+    Please don't make things up. Ask for more information when needed.
+    
+    Context:
+    {context}
+
+    Question:
+    {question}
+
+    Answer:
+    '''
+  print(prompt)
+
+  command = ['llm', '-m', 'orca-mini-3b', prompt]
+  process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+  content = ''
+  while True:
+    output = process.stdout.readline()
+    if output:
+      content = content + output
+    return_code = process.poll()
+    if return_code is not None:
+      break
+
+  msg = { 'role': 'assistant', 'content': content }
+  st.session_state.messages.append(msg)
+  st.chat_message("assistant").write(msg['content'])
