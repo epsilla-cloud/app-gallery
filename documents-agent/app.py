08cd2c74b5aa74489aa6854b26422ebdeee04504
richard-epsilla <richard@epsilla.com>
2023-10-31 17:33:40 +0800

    Chat UI

---
+++ documents-agent/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("💬 Document Agent")
+if "messages" not in st.session_state:
+  st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you about the documents?"}]
+
+
+for msg in st.session_state.messages:
+  st.chat_message(msg["role"]).write(msg["content"])
+
+if question := st.chat_input():
+  st.session_state.messages.append({"role": "user", "content": question})
+
+  st.chat_message("user").write(question)
+
+  msg = { 'role': 'assistant', 'content': '<THE RESPONSE>' }
+  st.session_state.messages.append(msg)
+  st.chat_message("assistant").write(msg['content'])
