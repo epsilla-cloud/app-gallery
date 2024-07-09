e52f1728a59d3efd95397771c82fa383d42e7b97
richard-epsilla <richard@epsilla.com>
2024-07-09 09:31:25 -0400

    frl

---
+++ demos/frl/header.js
@@ -0,0 +1,31 @@
+	add_HTML = 	"<header>" +
+				"<nav class=\"navbar navbar-expand-lg navbar-dark bg-blue\">" +
+					"<div class=\"container\">" +
+						"<a class=\"navbar-brand\" href=\"index.html\">" +
+							"<div class=\"logo\">FRL</div>" +
+							"Frontier Research Library" +
+						"</a>" +
+						"<button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNav\">" +
+							"<span class=\"navbar-toggler-icon\"></span>" +
+						"</button>" +
+						"<div class=\"collapse navbar-collapse\" id=\"navbarNav\">" +
+							"<ul class=\"navbar-nav ms-auto\">" +
+								"<li class=\"nav-item\">" +
+									"<a class=\"nav-link\" href=\"about.html\">About</a>" +
+								"</li>" +
+								"<li class=\"nav-item\">" +
+									"<a class=\"nav-link\" href=\"services.html\">Services</a>" +
+								"</li>" +
+								"<li class=\"nav-item\">" +
+									"<a class=\"nav-link\" href=\"guides.html\">Research Guides</a>" +
+								"</li>" +
+								"<li class=\"nav-item\">" +																
+									"<a class=\"nav-link\" href=\"special_collections.html\">Special Collections</a>" +
+								"</li>" +
+							"</ul>" +
+						"</div>" +
+					"</div>" +
+				"</nav>" +
+			"</header>";		
+			
+    document.writeln(add_HTML);
